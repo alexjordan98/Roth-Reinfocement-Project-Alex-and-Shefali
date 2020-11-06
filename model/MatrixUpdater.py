54773f00c7f53323eef7eb3c671813efb16c1980
alexjordan98 <alexjordan98@gmail.com>
2020-11-06 14:45:18 -0500

    finished itter

--- model/MatrixUpdater.py
+++ model/MatrixUpdater.py
@@ -1,19 +1,21 @@
 import quantumrandom
 from model import Matrix
-#This class creates all the different iterations of the game matrix
-class MatrixUpdater:
+from model import Cell
 
+
+# This class creates all the different iterations of the game matrix
+class MatrixUpdater:
     startState: Matrix
     iterations: int
     states = []
 
-    #Make whatever kind of iterator or matrix you want
+    # Make whatever kind of iterator or matrix you want
     def __init__(self, startState: Matrix, iterations: int):
 
         self.startState = startState
         self.iterations = iterations
 
-    #default matrix creator
+    # default matrix creator
     def __init__(self):
 
         self.iterations = 10
@@ -24,39 +26,58 @@ class MatrixUpdater:
         self.startState = m
 
 
-    # makes different instatiations of the matrix
-    def makeStates(self):
+    # updates matrix accorting to umber of itterations
+    def itter(self, m1: Matrix):
 
 
-        m1= self.startState
-        e1= m1.getEpsilon
 
-        self.states.add(m1)
+        i = 0
+
+        while i < self.iterations:
+            self.states.append(m1)
+            self.makeStates(m1)
+            i += 1
+
+    # updates the matrix once
+    def makeStates(self, m1: Matrix):
+
+        e1 = m1.getEpsilon
+        m1.makeMatrix()
 
 
-        u = quantumrandom.randfloat(0,1)
+
+        u = quantumrandom.randfloat(0, 1)
 
         list1 = m1.getL2
 
+        d = m1.getDelta()
+
+        delt = 1 - d
+
+
         # if no error
-        if u >e1:
+        if u > e1:
             self.setCellRange()
 
-            v = quantumrandom.randfloat(0,1)
+            v = quantumrandom.randfloat(0, 1)
+
+
+
 
             for list in list1:
                 for cell1 in list:
                     if v >= cell1.getMin() and v < cell1.getMax():
-
                         currWeight = cell1.getWeight
                         increase = cell1.getp1strat
                         newWeight = currWeight + increase
                         cell1.setWeight(newWeight)
+                    else:
+                        w = cell1.getWeight()
 
+                        cell1.setWeight(w * delt)
 
         # if error
         if u <= e1:
-
             x = m1.getRows
             y = m1.getCols
 
@@ -70,6 +91,14 @@ class MatrixUpdater:
             increase = choiceCell.getp1strat
             newWeight = currWeight + increase
 
+            for lis in list1:
+                for cel in lis:
+
+                    w = cel.getWeight()
+
+                    cel.setWeight(w * delt)
+
+
             choiceCell.setWeight(newWeight)
 
 
@@ -81,22 +110,21 @@ class MatrixUpdater:
         # i = number of rows
         i = self.startState.getl2.length()
         j = 0
-        while j < i :
-           l =  self.startState.getl2()[j]
-           j+=1
-           # k = number of columns
-           k = l.length()
-           m = 0
-           while m < k :
-               cell = l[m]
-               if m == 0:
-                   cell.setMin(0)
-                   cell.setMax(cell.getProb())
-               else :
-                   #set cells other than the first to their probability range
-                   lastCell = l[m-1]
-                   cell.setMin(lastCell.getMax())
-                   currentMin = cell.setMin(lastCell.getMax())
-                   cell.setMax(currentMin + cell.getProb())
-
-
+        while j < i:
+            l = self.startState.getl2()[j]
+            j += 1
+            # k = number of columns
+            k = l.length()
+            m = 0
+            while m < k:
+                cell = l[m]
+                if m == 0:
+                    cell.setMin(0)
+                    cell.setMax(cell.getProb())
+                else:
+                    # set cells other than the first to their probability range
+                    lastCell = l[m - 1]
+                    cell.setMin(lastCell.getMax())
+                    currentMin = cell.setMin(lastCell.getMax())
+                    cell.setMax(currentMin + cell.getProb())
+                m += 1
